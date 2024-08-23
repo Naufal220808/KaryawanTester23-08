2fd1ecdd3b07cdf5a0eb503795c8d30b6ee5558b
nayakanaufal <nayaka_aziz_33rpl@student.smktelkom-mlg.sch.id>
2024-08-23 14:05:04 +0700

    Add files via upload

---
+++ KaryawanTester.java
@@ -0,0 +1,23 @@
+//Driver class
+public class KaryawanTester {
+    public static void main(String[] args) {
+        //object
+        //Class object = new Constructor
+        Karyawan tia = new Karyawan(37, "Tia", "TU", "Bandung");
+        Karyawan ryuichi = new Karyawan(38, "Ryuichi", "BK", "Lamongan");
+        Karyawan tegar = new Karyawan(16, "Tegar", "Kesiswaan", "Jepang");
+        Karyawan burhan = new Karyawan(38, "Burhan", "Sarpra", "Tulungagung");
+        Karyawan Ibra = new Karyawan(67, "Ibra", "Kurikulum", "Amerika");
+
+        tia.print();
+        ryuichi.print();
+        tegar.print();
+        burhan.print();
+        Ibra.print();
+
+
+
+        }
+    
+
+}
